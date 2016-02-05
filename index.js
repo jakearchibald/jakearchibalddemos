475eb3c18c46d7d13e6f5bff014fdbf8059af70e
Jake Archibald <jaffathecake@gmail.com>
2016-02-05 16:56:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use('/static/', express.static(__dirname + '/public/static'));
+
+app.get('/', function(request, response) {
+  res.send('<p>some html</p>');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
