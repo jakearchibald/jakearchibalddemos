268b563863498879f19cbd89498ea3b654f1f182
Jake Archibald <jaffathecake@gmail.com>
2016-02-10 12:50:09 +0000

    Grid example, simpler routing

--- demos/progressive-css/index.js
+++ demos/progressive-css/index.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use('/css/:delay/', (req, res, next) => {
-  const delay = Number(req.params.delay) || 0;
-  setTimeout(next, delay * 1000);
-}, express.static(`${__dirname}/public/css/`));
-
-app.use('/async-css/:delay/', (req, res, next) => {
-  const delay = Number(req.params.delay) || 0;
-  setTimeout(next, delay * 1000);
-}, express.static(`${__dirname}/public/async-css/`));
-
-app.use('/async-css/:min/:max/', (req, res, next) => {
+app.use('/:min(\\d+)/:max(\\d+)/', (req, res, next) => {
   const min = Number(req.params.min) || 0;
   const max = Number(req.params.max) || 0;
   const delay = Math.random() * (max-min) + min;
   setTimeout(next, delay * 1000);
-}, express.static(`${__dirname}/public/async-css/`));
+}, express.static(`${__dirname}/public/`));
 
 app.use('/', express.static(`${__dirname}/public/`));
 
