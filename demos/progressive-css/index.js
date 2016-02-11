60e36c218e4c336f36aabe60df08f382cbfdbf58
Jake Archibald <jaffathecake@gmail.com>
2016-02-11 15:20:45 +0000

    Adding comments section

--- demos/progressive-css/index.js
+++ demos/progressive-css/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const staticConfig = {maxAge: '2s'};
 
 app.use('/:min(\\d+)/:max(\\d+)/', (req, res, next) => {
   const min = Number(req.params.min) || 0;
   const max = Number(req.params.max) || 0;
   const delay = Math.random() * (max-min) + min;
   setTimeout(next, delay * 1000);
-}, express.static(`${__dirname}/public/`));
+}, express.static(`${__dirname}/public/`, staticConfig));
 
-app.use('/', express.static(`${__dirname}/public/`));
+app.use('/', express.static(`${__dirname}/public/`, staticConfig));
 
 module.exports = app;
